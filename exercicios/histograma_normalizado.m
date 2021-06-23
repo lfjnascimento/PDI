9ab208cd7b047f9b3ffe9457db34b02e57a439aa
lfjnascimento <lfjnascimento@gmail.com>
2021-06-22 20:32:14 -0400

    add histograma e histograma_normaliado

---
+++ exercicios/histograma_normalizado.m
@@ -0,0 +1,40 @@
+close all;
+clc;
+clear all;
+
+
+function result= hist(M, Q)
+  result = zeros(1, Q+1);
+  [y x] = size(M);
+
+  for(i=1: 1: y)
+    for(j=1: 1: x)
+      result(M(i,j)+1)++;
+    endfor
+  endfor
+end
+
+img = [
+    [1, 5, 15, 1, 0],
+    [14, 12, 10, 8, 2],
+    [12, 10, 14, 7, 7],
+    [8, 9, 0, 10, 11]
+];
+n = 4;
+Q = (n^2)-1;
+
+subplot(1, 4, 1);
+imshow(img, []);
+
+x = (0: Q);
+subplot(1, 4, 2);
+stem(x, hist(img, Q));
+xlim([0 Q]);
+
+[m n] = size(img);
+subplot(1, 4, 3);
+stem(x, hist(img, Q)/(m*n));
+xlim([0 Q]);
+
+subplot(1, 4, 4);
+imshow(img/max(img(:)), []);
